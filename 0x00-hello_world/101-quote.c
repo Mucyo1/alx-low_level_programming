e036b92aec07858ae8e14ba065587a00be2d266a
Mucyo1 <mucyoprinceherve@example.com>
2023-03-18 01:12:31 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - function ...
+ * Return: 0
+ */
+int main(void)
+{
+	fwrite(2,
+			"and that piece of art is useful\" -Dora Korpa, 2015-10-19\n",
+			59);
+	return (1);
+}
