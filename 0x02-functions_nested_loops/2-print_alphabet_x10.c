6bf59e567f041c68b269ae14cdbd4a95c02fec57
Mucyo1 <mucyoprinceherve@example.com>
2023-03-22 10:16:39 -0700

    2-alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,3 +1,17 @@
 #include "main.h"
+/**
+ * print_alphabet_x10 - function that prints 10x alphabets
+ * returns 0
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char j;
 
-void print_alphabet_x10(void);
+	for (i = 1 ; i <= 10 ; i++)
+	{
+		for (j = 'a' ; j <= 'z' ; j++)
+			_putchar(j);
+		_putchar('\n');
+	}
+}
