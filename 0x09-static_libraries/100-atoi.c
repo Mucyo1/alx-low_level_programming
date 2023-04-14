a724b9cadf22235a3313d77a1031214283ce416d
Mucyo1 <mucyoprinceherve@example.com>
2023-04-14 12:36:36 -0700

    A library is not a luxury but one of the necessities of life

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,7 +1,29 @@
-#include <stdio.h>
+#include "main.h"
+
+/**
+ * _atoi - convert a string into an integer.
+ *
+ * @s: the string to use.
+ *
+ * Return: integer.
+ */
 
 int _atoi(char *s)
-
 {
-    return atoi(s);
+	int x = 1, v = 0;
+	unsigned int b = 0;
+
+	while (!(s[v] <= '9' && s[v] >= '0') && s[v] != '\0')
+	{
+		if (s[v] == '-')
+			x *= -1;
+		v++;
+	}
+	while (s[v] <= '9' && (s[v] >= '0' && s[v] != '\0'))
+	{
+		b = (b * 10) + (s[v] - '0');
+		v++;
+	}
+	b *= x;
+	return (b);
 }
