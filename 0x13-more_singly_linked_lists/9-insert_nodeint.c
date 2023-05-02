be0f09f9af51d001e636082fd6e99450620cfa64
Mucyo1 <mucyoprinceherve@example.com>
2023-05-02 04:47:10 -0700

    insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -28,7 +28,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (newN);
 	}
 
-	tmp = *head;
+	ptr = *head;
 	for (x = 0; x < idx - 1 && ptr != NULL; x++)
 		ptr = ptr->next;
 	if (ptr == NULL)
